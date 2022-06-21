8209c11c7dbe1216cd90d28fe2e520e1f13352ca
silentsoft <hyesung.lee@silentsoft.org>
2022-06-21 17:42:27 +0900

    Delete App.test.js

--- src/main/react/App.test.js
+++
@@ -1,9 +0,0 @@
-import {render} from "@testing-library/react";
-import App from "./App";
-
-describe("App", () => {
-    it("should render hello world", () => {
-        render(<App/>);
-        expect(document.body).toHaveTextContent("Hello, world!");
-    });
-});
